0ae96786d03bcc845387131ca546cce24ea60ad2
MurodjonT <murodjon.mmm.99@mail.ru>
2025-05-15 10:30:49 +0500

    Initial Commit

---
+++ Daromad.uz/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Daromad.uz
+//
+//  Created by Murodjon Turobov on 15/05/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Daromad.uz/Daromad_uzApp.swift
@@ -0,0 +1,17 @@
+//
+//  Daromad_uzApp.swift
+//  Daromad.uz
+//
+//  Created by Murodjon Turobov on 15/05/25.
+//
+
+import SwiftUI
+
+@main
+struct Daromad_uzApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
